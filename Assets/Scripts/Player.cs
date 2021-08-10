5cce13824e45fdd989adf0c61fa0e4a8bd4ebbbd
Ping nonhero <v6572000@gmail.com>
2021-08-10 13:13:07 +0800

    add ViewportToWorldPoint() to method of move()

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]float move_speed = 10f;
+    [SerializeField]float xmin,xmax,ymin,ymax;
+    [SerializeField]float pad = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Move_limit();
     }
-
+    private void Move_limit()
+    {
+        Camera game_camera = Camera.main;
+        xmin = game_camera.ViewportToWorldPoint(new Vector3(0,0,0)).x;
+        xmax = game_camera.ViewportToWorldPoint(new Vector3(1,0,0)).x;
+        ymin = game_camera.ViewportToWorldPoint(new Vector3(0,0,0)).y;
+        ymax = game_camera.ViewportToWorldPoint(new Vector3(0,1,0)).y;
+    }
     // Update is called once per frame
     void Update()
     {
@@ -20,8 +29,8 @@ public class Player : MonoBehaviour
     {
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * move_speed;
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * move_speed;
-        var new_Xpos = transform.position.x + deltaX;
-        var new_Ypos = transform.position.y + deltaY;
+        var new_Xpos = Mathf.Clamp((transform.position.x + deltaX), xmin+pad, xmax-pad);
+        var new_Ypos = Mathf.Clamp((transform.position.y + deltaY), ymin+pad, ymax-pad);
         transform.position = new Vector2(new_Xpos,new_Ypos);
     }
 }
