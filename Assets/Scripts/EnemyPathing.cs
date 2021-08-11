84a31414b57d36775801eead8eb45dd58504a496
Ping nonhero <v6572000@gmail.com>
2021-08-11 08:21:33 +0800

    add method of move() to Enemy Prefab

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -4,16 +4,36 @@ using UnityEngine;
 
 public class EnemyPathing : MonoBehaviour
 {
-    [SerializeField]List<Transform>waypoints;
+    [SerializeField]List<Transform> waypoints;
+    [SerializeField]float move_speed = 2f;
+    int waypoint_index = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = waypoints[waypoint_index].transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        if (waypoint_index <= waypoints.Count - 1)
+        {
+            var target_position = waypoints[waypoint_index].transform.position;
+            var movement_frame = move_speed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, target_position, movement_frame);
+            if (transform.position == target_position)
+            {
+                waypoint_index++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
