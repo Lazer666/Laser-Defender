7ff5b3ba304261bc3b670ed3748a0191a205f269
Ping nonhero <v6572000@gmail.com>
2021-08-16 23:17:54 +0800

    fixed bug and complete whole game

--- Assets/Scripts/WaveConfig.cs
+++ Assets/Scripts/WaveConfig.cs
@@ -8,11 +8,13 @@ public class WaveConfig : ScriptableObject
         [SerializeField]GameObject enemy_pre;
         [SerializeField]GameObject path_pre;
         [SerializeField]float time_spawns = 0.5f;
-        [SerializeField]float spawn_random = 0.3f;
         [SerializeField]int num_enemy = 5;
         [SerializeField]float move_speed = 2f;
 
-        public GameObject Get_Enemypre() { return enemy_pre; }
+        public GameObject Get_Enemypre()
+        {
+            return enemy_pre;
+        }
         public List<Transform> Get_waypoints()
         {
             var wave_waypoints = new List<Transform>();
@@ -22,8 +24,16 @@ public class WaveConfig : ScriptableObject
             }
             return wave_waypoints;
         }
-        public float Get_Timespawns() { return time_spawns; }
-        public float Get_spawnrandom() { return spawn_random; }
-        public int Get_numenemy() { return num_enemy; }
-        public float Get_movespeed() { return move_speed; }
+        public float Get_Timespawns()
+        {
+            return time_spawns;
+        }
+        public int Get_numenemy()
+        {
+            return num_enemy;
+        }
+        public float Get_movespeed()
+        {
+            return move_speed;
+        }
 }
